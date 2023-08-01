7604eaf7e0ef7d3e940a550e1dbd232a9349ef90
alhatmii <alhatmii.150@gmail.com>
2023-08-01 21:08:32 +0400

    Added comments to the Base Entityclass

--- src/main/java/com/ProjectManagementBoard/ProjectManagementBoard/Model/BaseEntity.java
+++ src/main/java/com/ProjectManagementBoard/ProjectManagementBoard/Model/BaseEntity.java
@@ -7,13 +7,15 @@ import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 
 import java.util.Date;
+// To auto generate Getter & Setter for Properties
 @Getter
 @Setter
 @MappedSuperclass // Main Class "Inheritance", To Make sure That No repetition In The Code.
-
 public class BaseEntity {
+    // Creates Date
     @CreatedDate
     Date createDate;
+    // Update up tp date
     @UpdateTimestamp
     Date updateDate;
     Boolean isActive;
