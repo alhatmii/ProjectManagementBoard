5237499f67bcac2d865cb8074ca1e21b5d987e25
alhatmii <alhatmii.150@gmail.com>
2023-08-01 22:23:41 +0400

    Added properties to the Cards class

--- src/main/java/com/ProjectManagementBoard/ProjectManagementBoard/Model/Cards.java
+++ src/main/java/com/ProjectManagementBoard/ProjectManagementBoard/Model/Cards.java
@@ -1,4 +1,30 @@
 package com.ProjectManagementBoard.ProjectManagementBoard.Model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Entity
+@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+@Table(name = "card")
+
 public class Cards extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    Integer section;
+
+    @ManyToOne
+    @JoinColumn(name = "board_id")
+    private Board board;
+
 }
