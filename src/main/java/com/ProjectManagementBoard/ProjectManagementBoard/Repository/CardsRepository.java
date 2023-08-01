25d5e401b95c9220aabdfc0824841c67a1f6c60a
alhatmii <alhatmii.150@gmail.com>
2023-08-01 23:24:52 +0400

    Extend CardsRepository from JpaRepository

--- src/main/java/com/ProjectManagementBoard/ProjectManagementBoard/Repository/CardsRepository.java
+++ src/main/java/com/ProjectManagementBoard/ProjectManagementBoard/Repository/CardsRepository.java
@@ -1,4 +1,9 @@
 package com.ProjectManagementBoard.ProjectManagementBoard.Repository;
 
-public interface CardsRepository {
+import com.ProjectManagementBoard.ProjectManagementBoard.Model.Cards;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CardsRepository extends JpaRepository<Cards, Long> {
 }
