e61e1d531d6921fc11958f1ae0eb034f0bc81ef8
alhatmii <alhatmii.150@gmail.com>
2023-08-01 21:03:50 +0400

    Added properties to the Base Entityclass

--- src/main/java/com/ProjectManagementBoard/ProjectManagementBoard/Model/BaseEntity.java
+++ src/main/java/com/ProjectManagementBoard/ProjectManagementBoard/Model/BaseEntity.java
@@ -1,4 +1,20 @@
 package com.ProjectManagementBoard.ProjectManagementBoard.Model;
 
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.util.Date;
+@Getter
+@Setter
+@MappedSuperclass // Main Class "Inheritance", To Make sure That No repetition In The Code.
+
 public class BaseEntity {
+    @CreatedDate
+    Date createDate;
+    @UpdateTimestamp
+    Date updateDate;
+    Boolean isActive;
 }
