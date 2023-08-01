20e279ba98d41cfe35a9d30703d7923e9700fda3
alhatmii <alhatmii.150@gmail.com>
2023-08-01 21:19:18 +0400

    Added properties to the Board class& Added new class called Map Converter to the model

--- src/main/java/com/ProjectManagementBoard/ProjectManagementBoard/Model/Board.java
+++ src/main/java/com/ProjectManagementBoard/ProjectManagementBoard/Model/Board.java
@@ -1,4 +1,34 @@
 package com.ProjectManagementBoard.ProjectManagementBoard.Model;
 
-public class Board {
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "board")
+public class Board extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true)
+    private Long id;
+
+    private String title;
+
+    @Convert(converter = MapConverter.class)
+    private Map<Integer, String> columns = Map.of(
+            1, "To do",
+            2, "In progress",
+            3, "Done"
+    );
+
+    @OneToMany(mappedBy = "board")
+    private List<Cards> cards;
+
 }
