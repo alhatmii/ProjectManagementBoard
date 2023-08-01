db3e624c8b0ba10102218a26bc7c7ba4d2cde20e
alhatmii <alhatmii.150@gmail.com>
2023-08-01 20:46:11 +0400

    first commit

---
+++ src/main/java/com/ProjectManagementBoard/ProjectManagementBoard/ProjectManagementBoardApplication.java
@@ -0,0 +1,13 @@
+package com.ProjectManagementBoard.ProjectManagementBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ProjectManagementBoard/ProjectManagementBoard/ProjectManagementBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProjectManagementBoard.ProjectManagementBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
