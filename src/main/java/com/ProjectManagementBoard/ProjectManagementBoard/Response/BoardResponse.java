e1d4dc011edf119aa37e019bf02c186815ee1219
Mohammed Al Hatmi <118106521+alhatmii@users.noreply.github.com>
2023-08-04 13:17:50 +0400

    Added properties to BoardResponse class

--- src/main/java/com/ProjectManagementBoard/ProjectManagementBoard/Response/BoardResponse.java
+++ src/main/java/com/ProjectManagementBoard/ProjectManagementBoard/Response/BoardResponse.java
@@ -1,4 +1,21 @@
 package com.ProjectManagementBoard.ProjectManagementBoard.Response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Map;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class BoardResponse {
+
+    private Long id;
+    private String title;
+    private Map<Integer, String> columns;
 }
+
+
