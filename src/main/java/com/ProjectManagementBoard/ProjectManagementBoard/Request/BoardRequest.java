53c3ffcb10015dc1d6cf4bcfb77e720b3cde8bdc
Mohammed Al Hatmi <118106521+alhatmii@users.noreply.github.com>
2023-08-04 13:15:27 +0400

    Added properties to BoardRequest class

--- src/main/java/com/ProjectManagementBoard/ProjectManagementBoard/Request/BoardRequest.java
+++ src/main/java/com/ProjectManagementBoard/ProjectManagementBoard/Request/BoardRequest.java
@@ -1,4 +1,31 @@
 package com.ProjectManagementBoard.ProjectManagementBoard.Request;
 
+import com.ProjectManagementBoard.ProjectManagementBoard.Model.Board;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Map;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class BoardRequest {
+    private Long id;
+    private String title;
+    private Map<Integer, String> columns;
+
+    public Board ConvertToBoard(){
+        Board board = new Board();
+
+        board.setId(this.getId());
+        board.setTitle(this.getTitle());
+        board.setColumns(this.getColumns());
+
+        return board;
+    }
 }
+
+
