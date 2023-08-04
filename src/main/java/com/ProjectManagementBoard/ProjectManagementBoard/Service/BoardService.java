b12c3a26e540959907450d4f6d5720b300cdbd0a
Mohammed Al Hatmi <118106521+alhatmii@users.noreply.github.com>
2023-08-04 13:23:37 +0400

    Added properties to BoardService class

--- src/main/java/com/ProjectManagementBoard/ProjectManagementBoard/Service/BoardService.java
+++ src/main/java/com/ProjectManagementBoard/ProjectManagementBoard/Service/BoardService.java
@@ -1,4 +1,38 @@
 package com.ProjectManagementBoard.ProjectManagementBoard.Service;
 
+import com.ProjectManagementBoard.ProjectManagementBoard.Model.Board;
+import com.ProjectManagementBoard.ProjectManagementBoard.Repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class BoardService {
+
+    @Autowired
+    BoardRepository boardRepository;
+
+    public String registerBoard(Board board){
+        boardRepository.save(board);
+        return "The board has been added";
+    }
+
+    public List<Board> getAllBoards(){
+        return boardRepository.findAll();
+    }
+
+    public Board getBoardById(Long id){
+        return boardRepository.findById(id).get();
+    }
+
+    public String deleteBoard(Long id){
+        boardRepository.deleteById(id);
+        return "The board has been deleted";
+    }
+
+    public Board saveBoard(Board board){
+        return boardRepository.save(board);
+    }
+
 }
