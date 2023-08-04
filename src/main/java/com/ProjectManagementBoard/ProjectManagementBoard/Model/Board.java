156fd53d862a7db0d5d4cbfb2ac3316b14181474
Mohammed Al Hatmi <118106521+alhatmii@users.noreply.github.com>
2023-08-04 09:49:56 +0400

    try to push

--- src/main/java/com/ProjectManagementBoard/ProjectManagementBoard/Model/Board.java
+++ src/main/java/com/ProjectManagementBoard/ProjectManagementBoard/Model/Board.java
@@ -20,7 +20,7 @@ public class Board extends BaseEntity {
     private Long id;
 
     private String title;
-// to convert
+// to convert to the column
     @Convert(converter = MapConverter.class)
     private Map<Integer, String> columns = Map.of(
             1, "To do",
